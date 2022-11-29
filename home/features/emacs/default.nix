7538ccbada58d5e3acdc08ad56d364c746db5fc3
Sinkerine <git@15cm.net>
2022-11-28 23:04:13 -0800

    Add user docker config

--- home/features/emacs/default.nix
+++ home/features/emacs/default.nix
@@ -2,7 +2,6 @@ args@{ pkgs, config, ... }:
 
 let customConfig = (import ./config.nix { inherit config; });
 in {
-  # Use the lucid instead of gtk to get a more stable daemon experience.
   programs.emacs = {
     enable = true;
     package = args.withArgs.packageOverride or pkgs.emacs-nox;
--- home/features/misc-dotfiles/default.nix
+++ home/features/misc-dotfiles/default.nix
@@ -3,4 +3,5 @@
 {
   home.file.".gitignore".source = ./gitignore;
   home.file.".ideavimrc".source = ./ideavimrc;
+  home.file.".config/docker/daemon.json" = ./config/docker/daemon.json;
 }
