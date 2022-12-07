e600d483739da458e9bc17937a786713e9e38be9
Sinkerine <git@15cm.net>
2022-12-07 00:39:46 -0800

    Use emacs pgtk on linux gui

--- home/users/sinkerine/common/linux-gui.nix
+++ home/users/sinkerine/common/linux-gui.nix
@@ -1,9 +1,8 @@
 args@{ pkgs, ... }: {
   imports = [
     # Essentials
-    # TODO: switch to the pkgs.emacsPgtkNativeComp when the nixfiles repo is stable. The Pgtk variant needs to compile from Emacs head and it takes a while.
     (import ../../../features/app/emacs
-      (args // { withArgs.packageOverride = pkgs.emacsNativeComp; }))
+      (args // { withArgs.packageOverride = pkgs.emacsPgtk; }))
     ../../../features/conf/ssh
     # XSession related
     ../../../features/conf/xsession
