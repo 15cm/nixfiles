ed342c243ff0d625afc14c7d2a37c300fa25a9f8
Sinkerine <git@15cm.net>
2022-12-09 01:04:13 -0800

    Add vim colors

--- home/features/app/vim/default.nix
+++ home/features/app/vim/default.nix
@@ -1,4 +1,4 @@
-{ lib, mylib, state, ... }:
+{ lib, mylib, state, pkgs, ... }:
 
 with lib;
 let
@@ -11,5 +11,16 @@ in {
       (templateFile "vimrc" templateData)
       builtins.readFile
     ];
+  };
+
+  home.file.".vim/colors/solarized.vim".source = pkgs.fetchurl {
+    url =
+      "https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim";
+    hash = "sha256-i2NgdR9IIGv0sKRN0v65xIr2VvJmLlv2K0fruHwncAQ=";
+  };
+  home.file.".vim/colors/Tomorrow-Night.vim".source = pkgs.fetchurl {
+    url =
+      "https://github.com/chriskempson/tomorrow-theme/raw/master/vim/colors/Tomorrow-Night.vim";
+    hash = "sha256-Nrn/IoBWCpsoI/bVBVbJeY82VxchrEqV8hYn3c7LIgY=";
   };
 }
