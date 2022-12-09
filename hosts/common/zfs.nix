6d83d882c8fe9cffc411879565716057ed5c0fbb
Sinkerine <git@15cm.net>
2022-12-09 00:53:21 -0800

    Replace grub with systemd-boot

--- hosts/common/zfs.nix
+++ hosts/common/zfs.nix
@@ -8,16 +8,11 @@
   boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
 
   # Bootloader
-  boot.loader.efi.efiSysMountPoint = "/boot";
-  boot.loader.efi.canTouchEfiVariables = false;
-  boot.loader.generationsDir.copyKernels = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.copyKernels = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.zfsSupport = true;
-  boot.loader.grub.devices = [ "nodev" ];
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 3;
+  };
+  boot.loader.efi.canTouchEfiVariables = true;
 
   services.zfs = { autoScrub.enable = true; };
 }
