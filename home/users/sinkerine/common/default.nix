2950f628b00ab51b3b2a10a84ab28d46ede3dcac
Sinkerine <git@15cm.net>
2022-12-11 23:32:53 -0800

    Allow passing args to nix switch alias

--- home/users/sinkerine/common/default.nix
+++ home/users/sinkerine/common/default.nix
@@ -8,12 +8,14 @@
 
   home.file."local/bin/switch-nix-home.sh".source =
     let homeFlakeUri = with nixinfo; "path:${projectRoot}#${configName}";
-    in pkgs.writeShellScript "switch-nix-home.sh"
-    "home-manager switch --impure --flake ${homeFlakeUri}";
+    in pkgs.writeShellScript "switch-nix-home.sh" ''
+      home-manager switch "$@" --impure --flake ${homeFlakeUri}
+    '';
   home.file."local/bin/switch-nix-os.sh".source =
     let osFlakeUri = with nixinfo; "path:${projectRoot}#${hostname}";
-    in pkgs.writeShellScript "switch-nix-os.sh"
-    "sudo nixos-rebuild switch --flake ${osFlakeUri}";
+    in pkgs.writeShellScript "switch-nix-os.sh" ''
+      sudo nixos-rebuild switch "$@" --flake ${osFlakeUri}
+    '';
 
   imports = [ ../../../common/baseline.nix ../../../features/app/gpg ];
 }
