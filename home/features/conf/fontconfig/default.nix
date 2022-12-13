bbffbf7bcc400960f140eeb53175f0eb6a2bd08b
Sinkerine <git@15cm.net>
2022-12-12 20:49:31 -0800

    Use system font and custom font config

--- home/features/conf/fontconfig/default.nix
+++ home/features/conf/fontconfig/default.nix
@@ -1,16 +1,5 @@
 { config, pkgs, ... }:
 
 {
-  fonts.fontconfig = { enable = true; };
-
-  # home.packages = with pkgs; [
-  #   noto-fonts
-  #   noto-fonts-cjk-sans
-  #   noto-fonts-cjk-serif
-  #   noto-fonts-emoji
-  #   inconsolata-lgc
-  #   font-awesome_4
-  #   dejavu_fonts
-  #   (nerdfonts.override { fonts = [ "Noto" "InconsolataLGC" ]; })
-  # ];
+  xdg.configFile."fontconfig/fonts.conf".source = ./fonts.conf;
 }
