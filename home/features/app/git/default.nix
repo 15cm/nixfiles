c7dfadff5cf5b268acfb21f884ae691fa75094e2
Sinkerine <git@15cm.net>
2022-12-16 18:59:55 -0800

    rebase by default in git pull

--- home/features/app/git/default.nix
+++ home/features/app/git/default.nix
@@ -5,8 +5,9 @@
     enable = true;
     userName = "Sinkerine";
     userEmail = "git@15cm.net";
-
     lfs = { enable = true; };
+
+    extraConfig = { pull.rebase = true; };
   };
 
   home.packages = with pkgs; [ git-secret ];
