380f1ea89000d6e2d766785b7dfa5028c9cbfdd1
Sinkerine <git@15cm.net>
2022-12-19 00:04:16 -0800

    Init sachi

---
+++ hosts/sachi/default.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, lib, mylib, hostname, ... }:
+
+with lib;
+
+{
+  system.stateVersion = "22.05";
+  imports = [
+    ./generated/hardware-configuration.nix
+    ./generated/extra-configuration.nix
+    ../common/baseline.nix
+    ../common/zfs.nix
+    ../common/users/sinkerine.nix
+    ../features/app/docker
+  ];
+
+  sops = {
+    defaultSopsFile = ./secrets.yaml;
+    secrets = { hashedPassword.neededForUsers = true; };
+    age = {
+      keyFile = "/keys/age/sachi.txt";
+      sshKeyPaths = [ ];
+    };
+    # https://github.com/Mic92/sops-nix/issues/167
+    gnupg.sshKeyPaths = [ ];
+  };
+
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    kbdInteractiveAuthentication = false;
+    permitRootLogin = "no";
+  };
+
+  networking = {
+    hostName = hostname;
+    domain = "mado.moe";
+    useDHCP = true;
+  };
+}
