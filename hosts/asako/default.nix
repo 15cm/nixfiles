0b9cf22742a182deea48deb751783eb5308f41fe
Sinkerine <git@15cm.net>
2022-12-20 12:03:13 -0800

    Reduce trackpoint sensitivity

--- hosts/asako/default.nix
+++ hosts/asako/default.nix
@@ -73,7 +73,7 @@ in {
   # Trackpoint
   hardware.trackpoint = {
     enable = true;
-    sensitivity = 255;
+    sensitivity = 200;
   };
   # ATTR{device/speed} is missing in z13 trackpoint so https://github.com/NixOS/nixpkgs/blob/9805c6163a99a8bfb99e09531e85cb1549899aad/nixos/modules/tasks/trackpoint.nix#LL80C4-L80C22 will fail.
   services.udev.extraRules = let cfg = config.hardware.trackpoint;
