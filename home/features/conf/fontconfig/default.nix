ffcbd3c29b2daed7950a5c034d403b0cbdb62b2f
Sinkerine <git@15cm.net>
2023-01-09 16:06:05 -0800

    Disable user font config in work

--- home/features/conf/fontconfig/default.nix
+++ home/features/conf/fontconfig/default.nix
@@ -1,22 +1,27 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, nixinfo, ... }:
 
-{
-  home.packages = with pkgs; [
-    font-manager
-    fontconfig
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    noto-fonts-emoji
-    font-awesome_4
-    emacs-all-the-icons-fonts
-    sarasa-gothic
-    sarasa-gothic-nerdfont
-    iosevka
-  ];
+with lib;
+mkMerge [
+  {
+    home.packages = with pkgs; [
+      font-manager
+      fontconfig
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-emoji
+      font-awesome_4
+      emacs-all-the-icons-fonts
+      sarasa-gothic
+      sarasa-gothic-nerdfont
+      iosevka
+    ];
 
-  xdg.configFile."fontconfig/conf.d/90-my-fonts.conf".source = ./fonts.conf;
-  # So that font manager can see the user installed fonts.
-  home.file.".local/share/fonts".source = "${config.home.path}/share/fonts";
-  fonts.fontconfig.enable = true;
-}
+    # So that font manager can see the user installed fonts.
+    home.file.".local/share/fonts".source = "${config.home.path}/share/fonts";
+    fonts.fontconfig.enable = true;
+  }
+  (mkIf (nixinfo.configName != "work@desktop") {
+    xdg.configFile."fontconfig/conf.d/90-my-fonts.conf".source = ./fonts.conf;
+  })
+]
