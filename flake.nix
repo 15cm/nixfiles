29e57d7610060799c20eb717dc9644b64d6f2568
Sinkerine <git@15cm.net>
2023-04-24 21:03:35 -0700

    Bump Hyprland version. Use Nvidia beta driver

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     deploy-rs.url = "github:serokell/deploy-rs";
     hyprland.url =
-      "github:hyprwm/Hyprland/2df0d034bc4a18fafb3524401eeeceaa6b23e753";
+      "github:hyprwm/Hyprland";
   };
 
   outputs = { self, nixpkgs, home-manager, nixgl, flake-utils, sops-nix, kmonad
