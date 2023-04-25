b4d3649ad3ce1d7637e294fa325428001110a95e
Sinkerine <git@15cm.net>
2023-04-24 20:06:43 -0700

    Switch to emacsUnstable as the latest tag is already emacs 29

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,8 +5,8 @@ with pkgs; {
   i3-quickterm = callPackage ./i3-quickterm { };
   ranger_devicons = callPackage ./ranger_devicons { };
   AriaNg = callPackage ./AriaNg { };
-  myEmacs = emacsGit;
+  myEmacs = emacsUnstable;
   myEmacs-nox = (emacsPackagesFor emacsGit-nox).emacsWithPackages
-      (epkgs: with epkgs; [ emacsql-sqlite ]);
+    (epkgs: with epkgs; [ emacsql-sqlite ]);
   sarasa-gothic-nerdfont = (callPackage ./sarasa-gothic-nerdfont { });
 }
