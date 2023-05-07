ff6df58beeba018001088824d514ab23fd6dd6df
Sinkerine <git@15cm.net>
2023-05-07 13:12:56 -0700

    Disable the NetworkManager-wait-online.enable systemd target that exits with an error

--- hosts/common/baseline.nix
+++ hosts/common/baseline.nix
@@ -122,4 +122,8 @@ with lib; {
       };
     };
   };
+
+  # https://github.com/NixOS/nixpkgs/issues/180175
+  systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
+  systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
 }
