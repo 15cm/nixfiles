406aeec4595d036eede5458d2ab2166646276eec
Sinkerine <git@15cm.net>
2023-05-07 11:54:53 -0700

    Bump flake lock versions

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,16 +5,14 @@ with nixpkgs.lib; {
   additions = self: _super: import ../pkgs { pkgs = self; };
 
   modifications = self: super: {
-    calibre = super.calibre.overrideAttrs (old: {
-      patches = old.patches ++ [
-        (super.fetchpatch {
-          name =
-            "0026-TypeError-HistoryLineEdit.__init__-got-an-unexpected.patch";
-          url =
-            "https://raw.githubusercontent.com/debian-calibre/calibre/master/debian/patches/0026-TypeError-HistoryLineEdit.__init__-got-an-unexpected.patch";
-          hash = "sha256-W9W6O2pbOOBk93BhYsVYhaqLs6ymDN82cjFKpltlyIc=";
-        })
-      ];
+    goldendict = super.goldendict.overrideAttrs (old: {
+      version = "2023-05-07";
+      src = super.fetchFromGitHub {
+        owner = "goldendict";
+        repo = "goldendict";
+        rev = "6f82f388d1a8d9e9d9dd1aef8dd38b370bbdca2e";
+        sha256 = "sha256-ORcBYqkloZNWk6RDXVGHfOhFzPI/7RlKNUWBYx+1PUY=";
+      };
     });
   };
 }
