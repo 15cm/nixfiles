f82e295444e824aab87fa22877d8d9547c6a5b05
Sinkerine <git@15cm.net>
2023-05-16 02:26:12 -0700

    Use emacsPgtk

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,7 +5,7 @@ with pkgs; {
   i3-quickterm = callPackage ./i3-quickterm { };
   ranger_devicons = callPackage ./ranger_devicons { };
   AriaNg = callPackage ./AriaNg { };
-  myEmacs = emacsUnstable;
+  myEmacs = emacsPgtk;
   myEmacs-nox = (emacsPackagesFor emacsGit-nox).emacsWithPackages
     (epkgs: with epkgs; [ emacsql-sqlite ]);
   sarasa-gothic-nerdfont = (callPackage ./sarasa-gothic-nerdfont { });
