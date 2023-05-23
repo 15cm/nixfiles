c9e3307a8a639193e748a77f7de87e5470113f16
Sinkerine <git@15cm.net>
2023-05-22 21:16:09 -0700

    [security] Allow wheel users to power control without root

--- hosts/common/baseline.nix
+++ hosts/common/baseline.nix
@@ -81,6 +81,26 @@ with lib; {
     '';
   };
 
+  security.polkit = {
+    enable = true;
+    extraConfig = ''
+      polkit.addRule(function(action, subject) {
+        if (
+          subject.isInGroup("wheel")
+            && (
+              action.id == "org.freedesktop.login1.reboot" ||
+              action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+              action.id == "org.freedesktop.login1.power-off" ||
+              action.id == "org.freedesktop.login1.power-off-multiple-sessions" ||
+            )
+          )
+        {
+          return polkit.Result.YES;
+        }
+      })
+    '';
+  };
+
   system.activationScripts.linkTzdata = ''
     if ! [ -L /usr/share/zoneinfo ]; then
       mkdir -p /usr/share
