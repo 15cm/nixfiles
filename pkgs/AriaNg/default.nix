0d94fa8a54523ac81a6e2903ea7fcaa321da4734
Sinkerine <git@15cm.net>
2023-06-04 23:27:14 -0700

    [flake] Bump AriaNg version

--- pkgs/AriaNg/default.nix
+++ pkgs/AriaNg/default.nix
@@ -2,14 +2,14 @@
 
 let
   pname = "AriaNg";
-  version = "1.3.2";
+  version = "1.3.6";
 in pkgs.stdenv.mkDerivation {
   inherit pname version;
   src = fetchzip {
     url =
       "https://github.com/mayswind/AriaNg/releases/download/${version}/AriaNg-${version}-AllInOne.zip";
     stripRoot = false;
-    hash = "sha256-eBdhFY0PgCBt86gyCHCBL9QJzOdgD9I9oj7CZoF8HIc=";
+    hash = "sha256-xDwo72vW+apIVxGNvcEWo7I9cKsUarXf06YH9KazxHw=";
   };
   phases = [ "installPhase" ];
   installPhase = ''
