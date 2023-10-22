3d6d3094b671324e79c4a2d31920371cd9b83c87
Sinkerine <git@15cm.net>
2023-10-22 16:41:15 -0700

    [misc] Fix emoji on firefox

--- home/modules/programs/fontconfig/default.nix
+++ home/modules/programs/fontconfig/default.nix
@@ -26,6 +26,7 @@ in {
         wqy_zenhei
         # Not sure why Font Manager doesn't work well with FontAwesome v6
         font-awesome_5
+        symbola
       ];
       # So that font manager can see the user installed fonts.
       home.file.".local/share/fonts".source = "${config.home.path}/share/fonts";
