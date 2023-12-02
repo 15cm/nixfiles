de67c14a3bb30f9ff4ba31e4f721f550364a5dfb
Sinkerine <git@15cm.net>
2023-12-02 11:25:30 -0800

    dunst: change mouse click to only left and right

--- home/modules/services/dunst/default.nix
+++ home/modules/services/dunst/default.nix
@@ -11,8 +11,7 @@ in {
       settings = {
         global = {
           mouse_left_click = "do_action,close_current";
-          mouse_middle_click = "close_current";
-          mouse_right_click = "context";
+          mouse_right_click = "do_action";
         };
       };
     };
