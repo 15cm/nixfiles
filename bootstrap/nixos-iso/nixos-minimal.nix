57427774b033c8ab68a75af56ff4ac2ded19305e
Sinkerine <git@15cm.net>
2023-12-03 02:04:08 -0800

    nixos-iso: minor changes

--- bootstrap/nixos-iso/nixos-minimal.nix
+++ bootstrap/nixos-iso/nixos-minimal.nix
@@ -8,10 +8,10 @@ with lib;
   # use the latest Linux kernel
   boot.kernelPackages = pkgs.zfs.latestCompatibleLinuxPackages;
 
-  boot.supportedFilesystems = mkForce [ "ext4" "fat32" "zfs" ];
+  boot.supportedFilesystems = [ "ext4" "fat32" "zfs" ];
 
   environment.systemPackages = with pkgs; [ ntfs3g ];
 
   networking.networkmanager.enable = true;
-  networking.wireless.enable = mkForce false;
+  networking.wireless.enable = mkImageMediaOverride false;
 }
