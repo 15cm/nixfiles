d5e2798bcf47d89e836701c9ee629214d3632ccf
Sinkerine <git@15cm.net>
2025-05-29 01:04:08 -0700

    [misc] Bump webos dev manager version

--- pkgs/webos-dev-manager/default.nix
+++ pkgs/webos-dev-manager/default.nix
@@ -1,11 +1,11 @@
 { appimageTools, fetchurl }:
 let
   pname = "webos-dev-manager";
-  version = "1.99.3";
+  version = "1.99.13";
 
   src = fetchurl {
     url =
       "https://github.com/webosbrew/dev-manager-desktop/releases/download/v${version}/${pname}_${version}_amd64.AppImage";
-    hash = "sha256-AZABETtuJ89TAqi93WgR1D+0OBd1fTnIIIx4THffr78=";
+    hash = "sha256-9jzdepZCiORX0nQCQenpFdOFsQaLWxa7/m4j0iCGlLI";
   };
 in appimageTools.wrapType2 { inherit pname version src; }
