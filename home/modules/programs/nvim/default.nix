ce607ef4c701cd411f0cab9743d9f149d9a0f0cc
Sinkerine <git@15cm.net>
2025-10-11 15:58:47 -0700

    [nvim] Add some basic plugins and lazy loading

--- home/modules/programs/nvim/default.nix
+++ home/modules/programs/nvim/default.nix
@@ -4,5 +4,35 @@ with lib;
 let cfg = config.my.programs.nvim;
 in {
   options.my.programs.nvim = { enable = mkEnableOption "Neo Vim"; };
-  config = mkIf cfg.enable { programs.nixvim = { enable = true; }; };
+  config = mkIf cfg.enable {
+    programs.nixvim = {
+      enable = true;
+      globals.mapleader = " ";
+      keymaps = [{
+        key = "<leader>ft";
+        action = "<cmd>NvimTreeToggle<CR>";
+      }];
+      plugins = {
+        "lz-n".enable = true;
+        nix = { enable = true; };
+        "nvim-tree" = {
+          enable = true;
+          lazyLoad = {
+            settings = {
+              cmd = "NvimTreeToggle";
+              keys = [ "<leader>ft" ];
+            };
+          };
+        };
+        "web-devicons".enable = true;
+        telescope = {
+          enable = true;
+          keymaps = {
+            "<A-x>" = { action = "commands"; };
+            "<leader>hx" = { action = "commands"; };
+          };
+        };
+      };
+    };
+  };
 }
