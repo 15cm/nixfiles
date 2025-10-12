dca99dd04949fabadd0c74f44b81a3eaa52ffa76
Sinkerine <git@15cm.net>
2025-10-11 22:20:08 -0700

    [nvim] Add yazi and project

--- home/modules/programs/nvim/default.nix
+++ home/modules/programs/nvim/default.nix
@@ -28,21 +28,46 @@ in {
             action = "<cmd>noh<CR>";
           }
           {
+            key = "<leader>;";
+            action = "<cmd>Yazi<CR>";
+          }
+          {
             key = "<leader>ft";
-            action = "<cmd>NvimTreeToggle<CR>";
+            action = "<cmd>Neotree toggle<CR>";
           }
           {
             key = "<leader>ff";
             action = "<cmd>Telescope find_files<CR>";
           }
           {
+            key = "<leader>fr";
+            action = "<cmd>Telescope oldfiles<CR>";
+          }
+          {
+            key = "<leader>ss";
+            action = "<cmd>Telescope live_grep<CR>";
+          }
+          {
             key = "<leader>bb";
             action = "<cmd>Telescope buffers<CR>";
           }
           {
-            mode = "n";
+            mode = [ "" "!" ];
+            key = "<A-c>";
+            action = "<cmd>Telescope yank_history<CR>";
+          }
+          {
+            mode = [ "" "!" ];
+            key = "<A-x>";
+            action = "<cmd>Telescope commands<CR>";
+          }
+          {
             key = "<leader>bn";
-            action = "<cmd>Telescope buffers<CR>";
+            action = "<cmd>enew<CR>";
+          }
+          {
+            key = "<leader>bd";
+            action = "<cmd>bp <BAR> bd #<CR>";
           }
           {
             key = "<leader>gg";
@@ -53,41 +78,90 @@ in {
             action = nixvimLib.mkRaw ''function() require("flash").jump() end'';
             options.desc = "Flash";
           }
+          {
+            mode = "c";
+            key = "<C-s>";
+            action =
+              nixvimLib.mkRaw ''function() require("flash").toggle() end'';
+            options.desc = "Toggle Flash Search";
+          }
+          {
+            key = "<leader>pp";
+            action = "<cmd>Telescope projects<CR>";
+          }
+          {
+            key = "<leader>pn";
+            action = "<cmd>ProjectAdd<CR>";
+          }
+          {
+            key = "<leader>pd";
+            action = "<cmd>ProjectDelete<CR>";
+          }
+          {
+            key = "<leader>p/";
+            action = "<cmd>ProjectFzf<CR>";
+          }
         ];
 
         plugins = {
           "lz-n".enable = true;
           nix = { enable = true; };
-          "nvim-tree" = {
-            enable = true;
-            autoClose = true;
-            openOnSetup = true;
-          };
+          "neo-tree" = { enable = true; };
           "web-devicons".enable = true;
           telescope = {
             enable = true;
-            keymaps = {
-              "<A-x>" = { action = "commands"; };
-              "<A-c>" = { action = "yank_history"; };
-              "<leader>hx" = { action = "commands"; };
-            };
+            extensions = { "file-browser".enable = true; };
           };
+          project-nvim = { enable = true; };
           yanky = {
             enable = true;
             enableTelescope = true;
           };
           neogit = {
             enable = true;
-            lazyLoad = {
-              settings = {
-                cmd = "Neogit";
-                keys = [ "<leader>gg" ];
-              };
-            };
+            lazyLoad = { settings = { cmd = "Neogit"; }; };
           };
           flash = { enable = true; };
           airline = { enable = true; };
+          fzf-lua = { enable = true; };
+          yazi = {
+            enable = true;
+            settings = {
+              open_for_directories = true;
+              keymaps = false;
+              clipboard_register = "";
+            };
+            lazyLoad = { settings = { cmd = "Yazi"; }; };
+            luaConfig.pre = ''
+              vim.g.loaded_netrw       = 1
+              vim.g.loaded_netrwPlugin = 1
+            '';
+          };
+          which-key = {
+            enable = true;
+            settings = {
+              spec = [
+                {
+                  __unkeyed-1 = "<leader>b";
+                  group = "Buffers";
+                }
+                {
+                  __unkeyed-2 = "<leader>f";
+                  group = "Files";
+                }
+                {
+                  __unkeyed-3 = "<leader>p";
+                  group = "Projects";
+                }
+              ];
+            };
+          };
         };
+        extraPlugins = with pkgs.vimPlugins; [ vim-rsi ];
+        extraConfigLua = ''
+          require("project").setup()
+          require("telescope").load_extension("projects")
+        '';
       };
     }
     (mkIf cfg.viAlias { programs.zsh.shellAliases = { vi = "nvim"; }; })
