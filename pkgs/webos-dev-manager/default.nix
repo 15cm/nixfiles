2691801b0d0f8117281e3fc1f1f5ebee126fbdae
Sinkerine <git@15cm.net>
2025-10-20 12:28:16 -0700

    [misc] Update webos manager to fix the Tauri build issue

--- pkgs/webos-dev-manager/default.nix
+++ pkgs/webos-dev-manager/default.nix
@@ -1,11 +1,11 @@
 { appimageTools, fetchurl }:
 let
   pname = "webos-dev-manager";
-  version = "1.99.13";
+  version = "1.99.16";
 
   src = fetchurl {
     url =
       "https://github.com/webosbrew/dev-manager-desktop/releases/download/v${version}/${pname}_${version}_amd64.AppImage";
-    hash = "sha256-9jzdepZCiORX0nQCQenpFdOFsQaLWxa7/m4j0iCGlLI";
+    hash = "sha256-1Eg8flL81vJXcGG9492tePqI4LpvEap2spuYtfIwAKU=";
   };
 in appimageTools.wrapType2 { inherit pname version src; }
