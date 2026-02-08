1babc331921f5bc0c04a2a8c961c83f14fbf3df4
Sinkerine <git@15cm.net>
2026-02-07 20:26:47 -0800

    [smb] Share storage/inbox

--- hosts/sachi/samba/default.nix
+++ hosts/sachi/samba/default.nix
@@ -1,6 +1,12 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
-with lib; {
+with lib;
+{
   sops.secrets.smbpasswd = {
     sopsFile = ./smbpasswd;
     format = "binary";
@@ -22,8 +28,7 @@ with lib; {
         "min receivefile size" = 16384;
         "aio read size" = 1;
         "aio write size" = 1;
-        "socket options" =
-          "IPTOS_LOWDELAY TCP_NODELAY IPTOS_THROUGHPUT SO_RCVBUF=131072 SO_SNDBUF=131072";
+        "socket options" = "IPTOS_LOWDELAY TCP_NODELAY IPTOS_THROUGHPUT SO_RCVBUF=131072 SO_SNDBUF=131072";
         "browseable" = "yes";
         "writeable" = "yes";
       };
@@ -32,21 +37,48 @@ with lib; {
 
     # Smb sharing doesn't work well with zfs properties.
     settings = {
-      "main-storage" = { path = "/pool/main/storage"; };
-      "main-music" = { path = "/pool/main/media/music"; };
-      "main-picture" = { path = "/pool/main/media/picture"; };
-      "main-photo" = { path = "/pool/main/media/photo"; };
-      "main-book" = { path = "/pool/main/media/book"; };
-      "main-sync" = { path = "/pool/main/sync"; };
-      "sub-aria2" = { path = "/pool/sub/download/aria2"; };
-      "sub-aria2-baidu" = { path = "/pool/sub/download/aria2-baidu"; };
-      "sub-arr" = { path = "/pool/sub/arr"; };
+      "main-storage" = {
+        path = "/pool/main/storage";
+      };
+      "main-storage-inbox" = {
+        path = "/pool/main/storage/inbox";
+      };
+      "main-music" = {
+        path = "/pool/main/media/music";
+      };
+      "main-picture" = {
+        path = "/pool/main/media/picture";
+      };
+      "main-photo" = {
+        path = "/pool/main/media/photo";
+      };
+      "main-book" = {
+        path = "/pool/main/media/book";
+      };
+      "main-sync" = {
+        path = "/pool/main/sync";
+      };
+      "sub-aria2" = {
+        path = "/pool/sub/download/aria2";
+      };
+      "sub-aria2-baidu" = {
+        path = "/pool/sub/download/aria2-baidu";
+      };
+      "sub-arr" = {
+        path = "/pool/sub/arr";
+      };
       "sub-torrent-download" = {
         path = "/pool/sub/download/torrent/download";
       };
-      "sub-download" = { path = "/pool/sub/download"; };
-      "sub-tvshow" = { path = "/pool/sub/media/tvshow"; };
-      "sub-movie" = { path = "/pool/sub/media/movie"; };
+      "sub-download" = {
+        path = "/pool/sub/download";
+      };
+      "sub-tvshow" = {
+        path = "/pool/sub/media/tvshow";
+      };
+      "sub-movie" = {
+        path = "/pool/sub/media/movie";
+      };
     };
   };
   # Initialize smb password following https://serverfault.com/questions/1104310/how-do-i-import-an-smbpasswd-file-into-a-different-samba-server
