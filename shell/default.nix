5636cb01ed048a192cba7d3f1bde0a026e08f2b4
Sinkerine <git@15cm.net>
2026-03-08 01:03:41 -0800

    Add text processing tools to default shell
    Add sed, grep, awk, and coreutils to provide standard Unix text
    processing utilities in the default nix development shell.

--- shell/default.nix
+++ shell/default.nix
@@ -12,6 +12,10 @@ in
 pkgs.mkShell {
   packages = [
     pkgs.nix
+    pkgs.gnused
+    pkgs.gnugrep
+    pkgs.gawk
+    pkgs.coreutils
     (jailed-agents.lib.${system}.makeJailedOpencode sharedConfig)
     (jailed-agents.lib.${system}.makeJailedClaudeCode sharedConfig)
   ];
