760bfda7bbcf03ecf2393f1a356c57bca5503ac4
Sinkerine <git@15cm.net>
2026-04-26 17:45:51 -0700

    fix(sachi): repair sops secrets and aliases

--- home/users/sinkerine/sachi/default.nix
+++ home/users/sinkerine/sachi/default.nix
@@ -7,6 +7,11 @@
 
   my.profiles.trusted.enable = true;
 
+  programs.zsh.shellAliases = {
+    snh = "switch-nix-home.sh";
+    sno = "switch-nix-os.sh";
+  };
+
   my.programs.baidupcs-go.enable = true;
 
   home.packages = with pkgs; [ nodejs ];
